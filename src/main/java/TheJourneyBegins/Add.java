a36c35db9d5bf92af49f6a5b42af1de08e70836c
Paul OBrien <PVOVideo@outlook.com>
2022-02-21 10:30:19 -0800

    add function and test

--- src/main/java/TheJourneyBegins/Add.java
+++ src/main/java/TheJourneyBegins/Add.java
@@ -1,4 +1,4 @@
-package intro;
+package TheJourneyBegins;
 
 public class Add {
     int solution(int param1, int param2) {
--- src/test/java/TheJourneyBegins/TheJourneyBeginsTest.java
+++ src/test/java/TheJourneyBegins/TheJourneyBeginsTest.java
@@ -1,5 +1,6 @@
-package intro;
+package TheJourneyBegins;
 
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -8,14 +9,42 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("The Journey Begins")
 public class TheJourneyBeginsTest {
 
-    class AddTest {
+    @Nested
+    @DisplayName("Add")
+    public class AddTest {
 
-        @DisplayName("Add")
+        Add aT = new Add();
+
         @Test
         public void AddT1() {
             Add addT = new Add();
             assertEquals(3, addT.solution(1, 2));
         }
 
-}
+        @Test
+        public void AddT2() {
+            assertEquals(1000, new Add().solution(0,1000));
+        }
+
+        @Test
+        public void AddT3() {
+            assertEquals(-37, new Add().solution(2, -39));
+        }
+
+        @Test
+        public void AddT4() {
+            assertEquals(199, new Add().solution(99,100));
+        }
+
+        @Test
+        public void AddT5() {
+            assertEquals(0, new Add().solution(-100, 100));
+        }
+
+        @Test
+        public void AddT6() {
+            assertEquals(-2000, aT.solution(-1000, -1000));
+        }
+    }
+
 }
