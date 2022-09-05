21db0d4dfb54abf63578e7c2acc119a0b01fa479
Paul O'Brien <PVOVideo@outlook.com>
2022-09-05 15:16:00 -0700

    Update LabOfNestedLoopsTests.java

--- src/test/java/TheCore/LabOfNestedLoops/LabOfNestedLoopsTests.java
+++ src/test/java/TheCore/LabOfNestedLoops/LabOfNestedLoopsTests.java
@@ -1,5 +1,6 @@
-package TheCore.LabOfNestedLoops;
+package TheCore.LabofNestedLoops;
 
+import TheCore.LabOfNestedLoops.CrosswordFormation;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,40 +10,70 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LabOfNestedLoopsTests {
 
     @Nested
-    @DisplayName("Square Digits Sequence")
-    public class SquareDigitsSequenceTest {
+    @DisplayName("Crossroad Formation Tests")
+    public class CrosswordFormationTests {
 
-        SquareDigitsSequence sDS = new SquareDigitsSequence();
+        CrosswordFormation cF = new CrosswordFormation();
 
         @Test
-        public void sDsTest1() {
-            assertEquals(9, sDS.solution(16));
+        public void cFTest1() {
+
+            String[] testArr = {"crossword",
+                    "square",
+                    "formation",
+                    "something"};
+
+            assertEquals(6, cF.solution(testArr));
         }
 
         @Test
-        public void sDsTest2() {
-            assertEquals(4, sDS.solution(103));
+        public void cFTest2() {
+
+            String[] testArr = {"anaesthetist",
+                    "thatch",
+                    "ethnics",
+                    "sabulous"};
+
+            assertEquals(0, cF.solution(testArr));
         }
 
         @Test
-        public void sDsTest3() {
-            assertEquals(4, sDS.solution(103));
+        public void cFTest3() {
+            String[] testArr = {"eternal",
+                    "texas",
+                    "chainsaw",
+                    "massacre"};
+
+            assertEquals(4, cF.solution(testArr));
         }
 
         @Test
-        public void sDsTest4() {
-            assertEquals(9, sDS.solution(16));
+        public void cFTest4() {
+            String[] testArr = {"africa",
+                    "america",
+                    "australia",
+                    "antarctica"};
+            assertEquals(62, cF.solution(testArr));
         }
 
         @Test
-        public void sDsTest5() {
-            assertEquals(9, sDS.solution(16));
+        public void cFTest5() {
+            String[] testArr = {"phenomenon",
+                    "remuneration",
+                    "particularly",
+                    "pronunciation"};
+
+            assertEquals(62, cF.solution(testArr));
         }
 
         @Test
-        public void sDsTest6() {
-            assertEquals(9, sDS.solution(16));
-        }
+        public void cFTest6() {
+            String[] testArr = {"onomatopoeia",
+                    "philosophical",
+                    "provocatively",
+                    "thesaurus"};
 
+            assertEquals(20, cF.solution(testArr));
+        }
     }
 }
