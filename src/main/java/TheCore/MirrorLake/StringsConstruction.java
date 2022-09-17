db67087ca2d4c305746b3ee507f94515c5a46b0e
Paul O'Brien <PVOVideo@outlook.com>
2022-09-17 14:10:56 -0700

    move parts to own functions, setup hashmap

--- src/main/java/TheCore/MirrorLake/StringsConstruction.java
+++ src/main/java/TheCore/MirrorLake/StringsConstruction.java
@@ -13,7 +13,6 @@ public class StringsConstruction {
         Stream<Character> charStream = IntStream // https://stackoverflow.com/questions/15590675/converting-char-array-to-list-in-java
                 .range(0, a.length())
                 .mapToObj(a::charAt);
-
         List<Character> aList = charStream.collect(Collectors.toList());
         HashMap<Character, Integer> aHashSet = new HashMap<>(); // needs to be a hashmap of letter and count
 
@@ -21,25 +20,30 @@ public class StringsConstruction {
             aHashSet.computeIfPresent(c, (k, v) -> v + 1);
             aHashSet.putIfAbsent(c, 1);
         }
-        HashMap<Character, Integer> testSet = new HashMap<>(aHashSet);
 
+        HashMap<Character, Integer> testSet = new HashMap<>(aHashSet); // make a copy of the hashmap, not a reference to the original
         for (int i = 0; i < b.length(); i++) { // need to substring to the length of String a and then run this.
             char theLetter = b.charAt(i);
             if (testSet.containsKey(theLetter)) {
-                if (testSet.get(theLetter) == 1) {
-                    testSet.remove(theLetter);
-                } else {
-                    testSet.computeIfPresent(theLetter, (k, v) -> v - 1);
-                }
-            } else {
-                testSet = new HashMap<>(aHashSet);
+                endCount = isMatch(b.subSequence(i, i + a.length()), testSet)
+                        ? endCount + 1
+                        : endCount;
             }
-
-            if (testSet.size() == 0) {
-                endCount++;
-                testSet = new HashMap<>(aHashSet);
-            }
+            testSet = new HashMap<>(aHashSet); // reset the set
         }
         return endCount;
+    }
+    boolean isMatch(CharSequence subS, HashMap<Character, Integer> testSet) {
+        for (int i = 0; i < subS.length(); i++) {
+            char theChar = subS.charAt(i);
+            if (testSet.containsKey(theChar)) {
+                if (testSet.get(theChar) == 1) {
+                    testSet.remove(theChar);
+                } else {
+                    testSet.computeIfPresent(theChar, (k, v) -> v - 1);
+                }
+            }
+        }
+        return testSet.size() == 0;
     }
 }
